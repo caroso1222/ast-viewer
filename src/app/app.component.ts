7b5eddb2f4ba2d93f8e669c5daffd7066edf9ac2
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2018-03-01 23:53:59 -0500

    feat: add linear scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -174,6 +174,8 @@ export class AppComponent  {
       },
     ]);
   }
+
+
 }
 
 export interface ASTNode {
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -24,8 +24,49 @@ export class AppService {
       oopNodeController.select();
       const el = oopNodeController.component.nodeElementRef.nativeElement;
       const offsetTop = el.offsetTop;
-      this.treeContainer.nativeElement.scrollTop = offsetTop - 200;
+      // this.treeContainer.nativeElement.scrollTop = offsetTop - 200;
+      this.scrollTo(
+        this.treeContainer.nativeElement,
+        offsetTop - 200,
+        150
+      );
     }
   }
+
+  // animation scrollTo(document.body, 0, 1250);
+
+  scrollTo(element, to, duration) {
+    const start = element.scrollTop,
+        change = to - start,
+        increment = 20;
+
+    let currentTime = 0;
+    const animateScroll = () => {
+        currentTime += increment;
+        const val = this.linear(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if (currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    };
+    animateScroll();
+}
+
+  // t = current time
+  // b = start value
+  // c = change in value
+  // d = duration
+  easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) {
+      return c / 2 * t * t + b;
+      }
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  };
+
+  linear = function (t, b, c, d) {
+    return (t /= d) * c + b;
+  };
 
 }
